faccbf8f5aa8b82f15b49c35a50ff2e17a60279c
19xino96 <19x.ino96@gmail.com>
2016-10-20 12:05:21 +0200

    Añadido JHolaMundo

---
+++ JHolaMundo/src/org/institutoserpis/ed/HolaMundo.java
@@ -0,0 +1,25 @@
+package org.institutoserpis.ed;
+
+import java.util.Scanner;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.print("Introduce tu nombre: ");
+//		String nombre = scanner.nextLine();
+//		System.out.print("Dime tu edad: ");
+//		String edad = scanner.nextLine();
+//		int edadSiguiente = Integer.parseInt(edad) + 1;
+//		System.out.println("eyy " + nombre + " el año que viene tendrás " + edadSiguiente + " años");
+		
+//		String s = "34";
+//		int i = (int)s;
+//		int i = Integer.parseInt(s);
+//		System.out.println("i=" + i);
+		
+//		Integer i = 25;
+//		String s = i.toString();		
+	}
+
+}
