86f2e33ff90917e281eb054ef130dfe64d96125c
19xino96 <19x.ino96@gmail.com>
2017-05-11 13:41:01 +0200

    Proyecto JEstadistica terminado (Eclipse). Se ha implementado el Main y se ha añadido un método para pasar vectores a cadenas

--- JEstadistica/src/org/institutoserpis/ed/Estadistica.java
+++ JEstadistica/src/org/institutoserpis/ed/Estadistica.java
@@ -1,18 +1,34 @@
 package org.institutoserpis.ed;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Estadistica {
 	
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String str;
+		boolean ctrl = false;
+		do {
+			System.out.print("Introduzca el tamaño del vector: ");
+			str = input.next();
+			if (str.matches("\\d+")) {
+				ctrl = true;
+			}
+		} while (!ctrl);
+		int[] vctr = generarVector(Integer.parseInt(str));
 		
+		System.out.println("\nVector: " + vectorToString(vctr));
+		System.out.println("\nValor máximo: " + valorMaximo(vctr)[0]
+				+ " (índice: " + valorMaximo(vctr)[1] + ")");
+		System.out.println("Valor mínimo: " + valorMinimo(vctr)[0]
+				+ " (índice: " + valorMinimo(vctr)[1] + ")");
+		System.out.println("Valor medio: " + valorMedio(vctr));
 	}
 
-	/**
+	/*
 	 * Genera un vector del tamaño indicado y lo rellena con números
 	 * aleatorios (valores del 1 al 10)
-	 * @param size La longitud del vector
-	 * @return El vector
 	 */
 	public static int[] generarVector(int size) {	
 		int[] vctr = new int[size];
@@ -23,11 +39,10 @@ public class Estadistica {
 		return vctr;
 	}
 	
-	/**
+	/*
 	 * Indica el valor máximo dentro de un vector y en qué índice se encuentra
-	 * el primer elemento con ese valor
-	 * @param vctr El vector
-	 * @return Vector de 2 elementos donde el primero indica el valor máximo y el segundo indica el índice
+	 * el primer elemento con ese valor. Devuelve un vector de 2 elementos donde el 
+	 * primero indica el valor máxmio y el segundo indica el índice.
 	 */
 	public static int[] valorMaximo(int[] vctr) {
 		int max = vctr[0];
@@ -41,11 +56,10 @@ public class Estadistica {
 		return new int[]{max, i};
 	}
 	
-	/**
+	/*
 	 * Indica el valor mínimo dentro de un vector y en qué índice se encuentra
-	 * el primer elemento con ese valor
-	 * @param vctr El vector
-	 * @return Vector de 2 elementos donde el primero indica el valor mínimo y el segundo indica el índice
+	 * el primer elemento con ese valor. Devuelve un vector de 2 elementos donde el 
+	 * primero indica el valor mínimo y el segundo indica el índice.
 	 */
 	public static int[] valorMinimo(int[] vctr) {
 		int min = vctr[0];
@@ -59,10 +73,8 @@ public class Estadistica {
 		return new int[]{min, i};
 	}
 	
-	/**
+	/*
 	 * Indica el valor medio obtenido de los elementos de un vector
-	 * @param vctr El vector
-	 * @return Valor medio
 	 */
 	public static double valorMedio(int[] vctr) {
 		double sum = 0;
@@ -70,5 +82,18 @@ public class Estadistica {
 			sum += vctr[i];
 		}
 		return sum/vctr.length;
+	}
+	
+	/*
+	 * Devuelve una cadena que lista los elementos del vector que se introduce como parámetro
+	 */
+	public static String vectorToString(int[] vctr) {
+		String str = "";
+		str = str.concat("{" + vctr[0]);
+		for (int i=1; i<vctr.length; i++) {
+			str = str.concat("," + vctr[i]);
+		}
+		str = str.concat("}");
+		return str;
 	}
 }
